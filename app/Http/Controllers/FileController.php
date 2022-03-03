15b7ff43b674f69d357f01ae8ba63a19da9887a8
marina <grubii.02@mail.ru>
2022-03-03 14:41:50 +0200

    first commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,50 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use Illuminate\Http\Request;
+use App\Models\File;
+  
+class FileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('fileUpload');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'files' => 'required',
+            'files.*' => 'required|mimes:pdf,xlx,csv|max:2048',
+        ]);
+      
+        $files = [];
+        if ($request->file('files')){
+            foreach($request->file('files') as $key => $file)
+            {
+                $fileName = time().rand(1,99).'.'.$file->extension();  
+                $file->move(public_path('uploads'), $fileName);
+                $files[]['name'] = $fileName;
+            }
+        }
+  
+        foreach ($files as $key => $file) {
+            File::create($file);
+        }
+     
+        return back()
+                ->with('success','You have successfully upload file.');
+   
+    }
+}
